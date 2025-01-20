6bedafcb633ca9b1a5a78993e814ffa47a08a2de
cemaltunbulduk <48567282+cemaltunbulduk@users.noreply.github.com>
2025-01-20 11:56:13 +0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,45 +1,21 @@
 import { ICollectionType, IConfig } from "./lib/app/types";
 
 const CONFIG: IConfig = {
-    coinDenom: "ustars",
-    name: "Embeddable House",
-    chainId: "elgafar-1",
-    createdDate: "2024-03-31T19:01:01.148Z",
-    modifiedDate: "2024-03-31T19:01:01.148Z",
-    id: "andromeda",
+    coinDenom: "uandr",
+    name: "NCA Exchange",
+    description: "New Chain Artistry (NCA) is a meme coin celebrating creativity and blockchain innovation, fostering fun and engagement in the crypto community",
+    chainId: "galileo-4",
+    createdDate: "2025-01-20T08:18:22.743Z",
+    modifiedDate: "2025-01-20T08:22:22.635Z",
+    id: "NCAExchange",
+    banner: "https://static.vecteezy.com/system/resources/previews/009/122/021/non_2x/nca-logo-nca-letter-nca-letter-logo-design-initials-nca-logo-linked-with-circle-and-uppercase-monogram-logo-nca-typography-for-technology-business-and-real-estate-brand-vector.jpg",
     collections: [
         {
-            auction:
-                "stars19xddx2zdky6d9jlsrqqyavfycr6pytwmn799vvqsu7fzd4dp7k4q9etd9q",
-            cw721: "stars15l5j0hf80ayj7yuhd6qjhlgwgmgy6apekwpe3k76lgltfhqge3cqemdnth",
-            name: "Auction Example",
-            type: ICollectionType.AUCTION,
-            id: "auction",
-            featured: "ANDR1"
-        },
-        {
-            marketplace:
-                "stars1gmjfd922u5jf8szjwad9k47ylsg0ftg6pvnaa7fplh8vrd0w4z0qa827u0",
-            cw721: "stars1cf0urzkvhph9nhxe5fv8rq25j8x683z3vcjfwc9ypv5qkar0jw2sv78dwu",
-            name: "Marketplace Example",
-            type: ICollectionType.MARKETPLACE,
-            id: "marketplace",
-        },
-        {
-            crowdfund:
-                "stars1a0el2qceyryftj9cx0ds4as3sgfu4ezj7mumtdp34v5c9qpswzaqw0qq38",
-            cw721: "stars1uj5tl3g26022ev9ym6x39xtv09hz3xvc7e3c5wthdpufxtwk7rsscs6jz4",
-            name: "Crowdfund Example",
-            type: ICollectionType.CROWDFUND,
-            id: "crowdfund",
-        },
-        {
-            exchange:
-                "stars1a9l4h3dkh6athk9z2qax63reg4ft5zs32sqdze6ruhdhxsdk8u6s4mpxyy",
-            cw20: "stars1ud5j93qj24zae3dgdugsqjkk9ez6hmg23c8gku4zygmvewn39x8sfmrw4k",
-            name: "Exchange Example",
-            type: ICollectionType.EXCHANGE,
-            id: "exchange",
+            exchange: "andr1has2u0gjvg3qw5d04mgpr25kr3zdcxyf5r80pgnfdtm7sk8frsfq3ekura",
+            cw20: "andr1z4y2fxx9y3g04ey03s6fjdr9earuqt3fcqvvln655aqt9zj270dq3qma5n",
+            name: "NCA Exchange",
+            type: "embeddables-exchange",
+            id: "embeddables-exchange-1",
         },
     ],
 };
